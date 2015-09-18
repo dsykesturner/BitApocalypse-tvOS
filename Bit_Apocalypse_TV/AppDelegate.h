2163caad72b362b0e0750a4367803eb2cf14b8e2
Daniel Sykes-Turner <dst@localz.co>
2015-09-18 21:03:03 +1000

    Initial Commit

---
+++ Bit_Apocalypse_TV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bit_Apocalypse_TV
+//
+//  Created by Daniel Sykes-Turner on 18/09/2015.
+//  Copyright © 2015 UniverseApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bit_Apocalypse_TV/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Bit_Apocalypse_TV
+//
+//  Created by Daniel Sykes-Turner on 18/09/2015.
+//  Copyright © 2015 UniverseApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
